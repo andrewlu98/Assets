bb87a1118dcf24450199cd3a0192c073f44505ab
a-krishnan7 <Amrith@calvisitor-10-105-158-79.calvisitor.1918.berkeley.edu>
2017-04-30 16:43:31 -0700

    Changes

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     // Copy our app's index.html to the build folder.
     new CopyWebpackPlugin([
       { from: 'app/index.html', to: "index.html" }
-    ])
+    ]),
     new CopyWebpackPlugin([
       { from: 'app/index2.html', to: "index2.html"}
     ])
